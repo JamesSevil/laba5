5851e35a25db133f221199255e63c3b61a80b1f8
JamesSevil <jsclassified24@gmail.com>
2024-05-25 15:26:29 +0700

    Переделал

--- zad4.cpp
+++ zad4.cpp
@@ -6,55 +6,60 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Rus");
+    setlocale(LC_ALL, "Russian");
     vector<pair<int, string>> schedule;
 
-    int N;
-    cout << "Введите число N: ";
-    cin >> N;
+    while (true) {
+        int N;
+        cout << "Введите число операций: ";
+        cin >> N;
 
-    for (int i = 0; i < N; ++i) {
         string operation;
-        cout << "Введите операцию: ";
-        cin >> operation;
+        for (int i = 0; i < N; ++i) {
+            cout << "Введите операцию: ";
+            cin >> operation;
 
-        if (operation == "CLASS") {
-            string discipline;
-            int day;
-            cout << "Введите номер дня и название предмета: ";
-            cin >> day >> discipline;
-            schedule.push_back(make_pair(day, discipline)); // Добавляем занятие в расписание
-        } else if (operation == "NEXT") {
-            // Переносим пары на предпоследний день текущего месяца при смене месяца
-            for (auto& cls : schedule) {
-                if (cls.first == 28 || cls.first == 29 || cls.first == 30 || cls.first == 31) {
-                    cls.first = 27;
-                } else {
-                    ++cls.first;
+            if (operation == "CLASS") {
+                string discipline;
+                int day;
+                cout << "Введите номер дня и название предмета: ";
+                cin >> day >> discipline;
+                schedule.push_back(make_pair(day, discipline)); // Добавляем занятие в расписание
+            } else if (operation == "NEXT") {
+                int lastDay = 29; // Предпоследний день текущего месяца
+
+                for (auto& cls : schedule) {
+                    cls.first = lastDay; // Переносим все занятия на предпоследний день текущего месяца
                 }
-            }
-            cout << "Переход на следующий месяц выполнен." << endl;
-        } else if (operation == "VIEW") {
-            int day;
-            cout << "Введите номер дня: ";
-            cin >> day;
+                cout << "Перенос всех занятий на предпоследний день текущего месяца выполнен." << endl;
+            } else if (operation == "VIEW") {
+                int day;
+                cout << "Введите номер дня: ";
+                cin >> day;
 
-            // Используем алгоритм find_if для поиска первого занятия на указанный день
-            auto it = find_if(schedule.begin(), schedule.end(), [day](const pair<int, string>& cls) {
-                return cls.first == day;
-            });
+                // Используем алгоритм find_if для поиска первого занятия на указанный день
+                auto it = find_if(schedule.begin(), schedule.end(), [day](const pair<int, string>& cls) {
+                    return cls.first == day;
+                });
 
-            cout << "В " << day << " день ";
-            if (it != schedule.end()) { // Если занятия нашлись на указанный день
-                cout << "1 занятие в университете: " << it->second;
-                ++it;
-                while (it != schedule.end() && it->first == day) { // Выводим все занятия на этот день
-                    cout << ", " << it->second;
+                cout << "В " << day << " день ";
+                if (it != schedule.end()) { // Если занятия нашлись на указанный день
+                    cout << "1 занятие в университете: " << it->second;
                     ++it;
+                    while (it != schedule.end() && it->first == day) { // Выводим все занятия на этот день
+                        cout << ", " << it->second;
+                        ++it;
+                    }
+                } else {
+                    cout << "У нас свободный день!"; // Если занятий нет на указанный день
                 }
-            } else {
-                cout << "У нас свободный день!" << endl; // Если занятий нет на указанный день
+                cout << endl;
+            } else if (operation == "EXIT") {
+                cout << "Программа завершает работу." << endl;
+                return 0;
             }
         }
     }
+
+    return 0;
 }
