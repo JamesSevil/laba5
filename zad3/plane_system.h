72aba40d642118334eee5bd94393efa9b5c6b940
JamesSevil <jsclassified24@gmail.com>
2024-08-09 12:09:18 +0700

    plus

--- zad3/plane_system.h
+++ zad3/plane_system.h
@@ -9,7 +9,7 @@ using namespace std;
 
 void create_plane(map<string, vector<string>>& kal);
 void planes_for_town(const map<string, vector<string>>& kal, const string& town);
-void towns_for_plane(const map<string, vector<string>>& kal, int plane);
+void towns_for_plane(const map<string, vector<string>>& kal, string plane);
 void planes(const map<string, vector<string>>& kal);
 
 enum class Type {
